85cd0ac7af647df79aba57c0f4f2dc4b6586f762
VldislavKh <vkh123w@gmail.com>
2023-08-30 15:01:05 +0300

    Добавлено кэширование в UserService

--- MuzShop/Services/UserService.cs
+++ MuzShop/Services/UserService.cs
@@ -1,6 +1,7 @@
 ﻿using Domain.Entities;
 using Domain.Infrastructure;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 using MuzShop.ExceptionMiddleware.CustomExceptions;
 using MuzShop.Interfaces.ServiceInterfaces;
 using System.Security.Cryptography;
@@ -11,10 +12,12 @@ namespace MuzShop.Services
     public class UserService : IUserService
     {
         private readonly ApplicationContext _context;
+        private readonly IMemoryCache _cache;
 
-        public UserService(ApplicationContext context)
+        public UserService(ApplicationContext context, IMemoryCache cache)
         {
             _context = context;
+            _cache = cache;
         }
 
         public async Task<Guid> AddAsync(string name, string password, Guid roleId)
@@ -29,7 +32,15 @@ namespace MuzShop.Services
             user.Role = role;
             
             await _context.AddAsync(user);
-            await _context.SaveChangesAsync();
+            int n = await _context.SaveChangesAsync();
+
+            if (n > 0)
+            {
+                _cache.Set(user.Id, user, new MemoryCacheEntryOptions
+                {
+                    AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5)
+                });
+            }
             return user.Id;
         }
 
@@ -39,6 +50,7 @@ namespace MuzShop.Services
                ?? throw new NotFoundException(nameof(_context.Users), userId);
 
             _context.Remove(user);
+            _cache.Remove(user.Id);
             await _context.SaveChangesAsync();
         }
 
@@ -56,14 +68,31 @@ namespace MuzShop.Services
             user.Role = role;
 
             _context.Update(user);
-            await _context.SaveChangesAsync();
+            int n = await _context.SaveChangesAsync();
+
+            if (n > 0)
+            {
+                _cache.Set(user.Id, user, new MemoryCacheEntryOptions
+                {
+                    AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5)
+                });
+            }
             return user.Id;
         }
 
         public async Task<User> GetAsync(Guid userId)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(u => u.Id == userId)
-               ?? throw new NotFoundException(nameof(_context.Users), userId);
+            User user = null;
+            if (!_cache.TryGetValue(userId, out user))
+            {
+                user = await _context.Users.SingleOrDefaultAsync(u => u.Id == userId)
+                    ?? throw new NotFoundException(nameof(_context.Users), userId);
+
+                _cache.Set(user.Id, user, new MemoryCacheEntryOptions
+                {
+                    AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5)
+                });
+            }
 
             return user;
         }
